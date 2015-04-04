0bd8ba4d11d347289403e4bc0d5f45850bae1465
kaalpurush <kaalbd@gmail.com>
2015-04-04 23:56:48 +0600

    fix

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ startKommander = function() {
 		kommand.run(6969, "0.0.0.0", false);
 		console.log("kommander is running...\nwaiting for kommands...")
 		kommand.on('data', function(cmd) {
-			console.log("kommand:" cmd);
+			console.log("kommand:", cmd);
 			cmd = cmd.toLowerCase();
 
 			tessel.led[1].write(1);
